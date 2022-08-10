01b735961927f9be719effccd45fe15ffb1c248c
Жанна Буковцева <jbukovtseva@mail.ru>
2022-08-10 12:17:54 +0300

    Trask1

---
+++ Trask1/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(100, 1000); // Случайное значение на вход
+Console.Write($"{number} -> ");
+
+// Console.Write("Введите трехзначное число: "); // Ввод значения пользователем 
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = (number % 100) / 10;
+
+Console.Write(secondNumber);
